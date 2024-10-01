2fff49a3f250ef968877ecbd04cb138978c92aeb
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-10-01 14:29:55 +0200

    updates

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,18 +1,12 @@
 import { Header } from "../components/shared/Header/Header";
 import { Insta } from "components/shared/Insta/Insta";
 import { Footer } from "components/shared/Footer/Footer";
-import { motion } from "framer-motion";
 export const Layout = ({ children }) => {
   return (
     <>
       <header className="header">
         <Header />
       </header>
-      <motion.div
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeOut", duration: 1 }}
-      >
         <main className="content">
           {children}
           <Insta />
@@ -20,7 +14,6 @@ export const Layout = ({ children }) => {
         <footer className="footer">
           <Footer />
         </footer>
-      </motion.div>
     </>
   );
 };
