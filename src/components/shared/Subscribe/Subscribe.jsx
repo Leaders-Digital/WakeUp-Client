673248334a4edb7d90036390052abe4e9d7f608d
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-10-24 11:04:21 +0200

    updates pictures

--- src/components/shared/Subscribe/Subscribe.jsx
+++ src/components/shared/Subscribe/Subscribe.jsx
@@ -4,6 +4,7 @@ import toast, { Toaster } from "react-hot-toast";
 
 export const Subscribe = () => {
   const [email, setEmail] = useState("");
+
   const isValidEmail = (email) => {
     const trimmedEmail = email.trim();
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -13,28 +14,29 @@ export const Subscribe = () => {
   const handleSubmit = async () => {
     if (!isValidEmail(email)) {
       toast.error("Veuillez entrer une adresse e-mail valide.");
-      return; // Exit the function if the email is invalid
+      return; // Quitter la fonction si l'e-mail est invalide
     }
     try {
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API_KEY}api/subscribe`,
-        { email },  // Data being sent in the body of the request
+        { email }, // Données envoyées dans le corps de la requête
         {
           headers: {
-            'x-api-key': process.env.NEXT_PUBLIC_KEY, // Send the API key in the request header
+            'x-api-key': process.env.NEXT_PUBLIC_KEY, // Envoyer la clé API dans l'en-tête de la requête
           },
         }
       );
-      setEmail(""); // Clear the email field
+      setEmail(""); // Vider le champ e-mail
       toast.success(response.data.message);
     } catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
     }
   };
+
   return (
     <>
-      {/* <!-- BEGIN SUBSCRIBE --> */}
+      {/* <!-- DÉBUT ABONNEMENT --> */}
       <div className="subscribe">
         <Toaster />
         <div className="wrapper">
@@ -43,30 +45,32 @@ export const Subscribe = () => {
               <img
                 src="/assets/img/subscribe-img.png"
                 className="js-img"
+                width={400}
+                height={300}
                 alt=""
               />
             </div>
             <div className="toform">
-              <h3>Stay in touch</h3>
-              <p>Nourish your skin with toxin-free cosmetic roducts.</p>
+              <h3>Restez en contact</h3>
+              <p>Soignez votre peau avec des produits cosmétiques sans toxines.</p>
               <div className="box-field__row">
                 <div className="box-field">
                   <input
                     type="email"
                     className="form-control"
-                    placeholder="Enter your email"
+                    placeholder="Entrez votre e-mail"
                     onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <button onClick={handleSubmit} className="btn">
-                  subscribe
+                  s'abonner
                 </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <!-- SUBSCRIBE EOF   --> */}
+      {/* <!-- FIN ABONNEMENT --> */}
     </>
   );
 };
