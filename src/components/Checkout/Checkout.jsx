d9da66ee72eb95d09c65d3e8d3e8af9686e9965c
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2025-12-19 14:15:16 +0100

    no tva

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -68,10 +68,6 @@ export const Checkout = () => {
   }, []);
 
   const totalWithDiscount = promo ? total - (total * promo) / 100 : total;
-  const deliveryFee = 8;
-  const subtotal = totalWithDiscount + deliveryFee;
-  const tva = subtotal * 0.19; // 19% TVA
-  const finalTotal = subtotal + tva;
   const listeDesProduits = [];
   const listeDesPack = [];
 
@@ -144,7 +140,7 @@ export const Checkout = () => {
           ...data,
           listeDesProduits,
           listeDesPack,
-          prixTotal: finalTotal,
+          prixTotal: totalWithDiscount,
         }, // Data being sent in the body of the request
         {
           headers: {
@@ -169,7 +165,7 @@ export const Checkout = () => {
   const onlinePayment = async () => {
     try {
       let orderid = await handleCreateOrder("pay");
-      let totalwithDilevery = finalTotal * 1000;
+      let totalwithDilevery = (totalWithDiscount + 8) * 1000;
 
       const paymentData = {
         receiverWalletId: "6721f70f82402c76c27e7fd7",
