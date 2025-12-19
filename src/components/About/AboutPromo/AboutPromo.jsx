b059a683fe97b197c18301437d6799d4f2bceca7
Punk010 <mohamed.amine.atouii@gmail.com>
2025-12-19 11:54:46 +0100

    carousel

--- src/components/About/AboutPromo/AboutPromo.jsx
+++ src/components/About/AboutPromo/AboutPromo.jsx
@@ -2,14 +2,49 @@ import { PromoVideo } from 'components/shared/PromoVideo/PromoVideo';
 import { SectionTitle } from 'components/shared/SectionTitle/SectionTitle';
 import { useState } from 'react';
 import PromoNumberData from 'data/promoNumber/promoNumber';
+import Slider from 'react-slick';
+import {
+  SlickArrowPrev,
+  SlickArrowNext,
+} from 'components/utils/SlickArrows/SlickArrows';
 
 export const AboutPromo = () => {
-  const [play, setPlay] = useState(false);
+  const [playingVideos, setPlayingVideos] = useState({});
 
   const promoNumber = [...PromoNumberData];
-  const url = play
-    ? 'https://www.youtube.com/embed/atLWuquNY80'
-    : '';
+  
+  const videos = [
+    {
+      id: 1,
+      url: 'https://www.youtube.com/embed/atLWuquNY80',
+      image: '/assets/img/promo-video-img.jpg',
+    },
+    {
+      id: 2,
+      url: 'https://www.youtube.com/embed/bgbnCCWmBRE',
+      image: '/assets/img/promo-video-img.jpg',
+    },
+  ];
+
+  const handleVideoPlay = (videoId) => {
+    setPlayingVideos((prev) => ({
+      ...prev,
+      [videoId]: true,
+    }));
+  };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <SlickArrowPrev />,
+    nextArrow: <SlickArrowNext />,
+    lazyLoad: false,
+  };
+
   return (
     <>
       {/* <!-- BEGIN PROMO VIDEO --> */}
@@ -22,12 +57,21 @@ export const AboutPromo = () => {
           />
 
           <div className='promo-video__block'>
-            <PromoVideo
-              videoUrl={url}
-              play={play}
-              onVideoPlay={() => setPlay(true)}
-              image='/assets/img/promo-video-img.jpg'
-            />
+            <Slider {...settings}>
+              {videos.map((video) => {
+                const isPlaying = playingVideos[video.id] || false;
+                return (
+                  <div key={video.id} style={{ position: 'relative', width: '100%', height: '550px' }}>
+                    <PromoVideo
+                      videoUrl={isPlaying ? video.url : ''}
+                      play={isPlaying}
+                      onVideoPlay={() => handleVideoPlay(video.id)}
+                      image={video.image}
+                    />
+                  </div>
+                );
+              })}
+            </Slider>
           </div>
 
         </div>
